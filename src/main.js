f9887b70623b971ec5210d46458ea456e40eaa83
Tomonori Mine <tomonori.mine.biz@gmail.com>
2021-09-09 13:31:07 +0900

    routerの共同を修正

--- src/main.js
+++ src/main.js
@@ -2,12 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+createApp(App).use(router).mount('#app')
 
-// createApp({
-//     //    router,
-//     render: h => h(App)
-// }).mount('#app')
 
--- src/router.js
+++ src/router.js
@@ -1,15 +1,21 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createWebHistory, createRouter } from "vue-router";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
 
 const routes = [
-    { path: '/Page1', component: Page1 },
-    { path: '/Page2', component: Page2 },
+    {
+        path: '/Page1',
+        component: Page1
+    },
+    {
+        path: '/Page2',
+        component: Page2
+    },
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes,
-})
+});
 
 export default router;
