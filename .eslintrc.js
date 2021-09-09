03c31e0bc142fef6c532ee9f2dfee0699bd8f908
Tomonori Mine <tomonori.mine.biz@gmail.com>
2021-09-09 14:01:50 +0900

    eslint修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,30 @@ module.exports = {
     "es6": true
   },
   "extends": [
-    "plugin:vue/vue3-recommended"
-  ]
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "plugin:vue-types/recommended",
+    "plugin:prettier/recommended"
+  ],
+  "plugins": [
+    "vue"
+  ],
+  "parser": "vue-eslint-parser",
+  "parserOptions": {
+    "sourceType": "module",
+    "parser": "babel-eslint"
+  },
+  "root": true,
+  "rules": {
+    "prettier/prettier": 0,
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "always"
+    }],
+    "vue-types/require-default-prop": 0,
+    "vue/attribute-hyphenation": 0,
+    "vue/no-parsing-error": 0,
+    "no-extra-parens": 1,
+    "no-multi-spaces": 2
+  }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <AppHeader />
   <div>
+    <AppHeader />
     <router-view />
   </div>
 </template>
