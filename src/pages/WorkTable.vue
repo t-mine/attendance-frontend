4aa6f14b46f335030f797b25a0415b5ed41af323
Tomonori Mine <tomonori.mine.biz@gmail.com>
2021-09-11 18:50:08 +0900

    勤務表画面のレイアウトを修正

--- src/pages/WorkTable.vue
+++ src/pages/WorkTable.vue
@@ -3,11 +3,15 @@
     <!-- 勤務表の上部 -->
     <table class="table">
       <tr>
-        <th style="width: 2%">2021</th>
-        <th style="width: 1%">年</th>
-        <th style="width: 1%">8</th>
-        <th style="width: 1%">月</th>
-        <th style="width: 95%" class="text-end">田中 太郎</th>
+        <th style="width: 5%">
+          <input class="form-control text-center" type="text" value="2021" />
+        </th>
+        <th style="width: 1%" class="align-middle">年</th>
+        <th style="width: 3%">
+          <input class="form-control text-center" type="text" value="8" />
+        </th>
+        <th style="width: 1%" class="align-middle">月</th>
+        <th style="width: 90%" class="text-end align-middle">田中 太郎</th>
       </tr>
     </table>
 
@@ -15,30 +19,35 @@
     <table class="table table-bordered table-secondary">
       <tr class="table-dark">
         <th style="width: 2%">日</th>
+        <th style="width: 4%">曜日</th>
         <th style="width: 10%">開始時刻</th>
         <th style="width: 10%">終了時刻</th>
-        <th style="width: 78%">メモ</th>
+        <th style="width: 74%">メモ</th>
       </tr>
       <tr>
         <td>1</td>
+        <td>月</td>
         <td>09:00</td>
         <td>18:00</td>
         <td></td>
       </tr>
       <tr>
-        <td>1</td>
+        <td>2</td>
+        <td>火</td>
         <td>09:00</td>
         <td>18:00</td>
         <td></td>
       </tr>
       <tr>
-        <td>1</td>
+        <td>3</td>
+        <td>水</td>
         <td>09:00</td>
         <td>18:00</td>
         <td></td>
       </tr>
       <tr>
-        <td>1</td>
+        <td>4</td>
+        <td>木</td>
         <td>09:00</td>
         <td>18:00</td>
         <td></td>
